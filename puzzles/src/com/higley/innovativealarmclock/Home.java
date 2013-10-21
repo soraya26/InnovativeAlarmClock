7fdc985908b70bfbed644ca6961ccbfd4dc8dbee
Chris Higley <higleyc@gmail.com>
2013-10-20 21:59:09 -0400

    Added simple math puzzle capability

--- puzzles/src/com/higley/innovativealarmclock/Home.java
+++ puzzles/src/com/higley/innovativealarmclock/Home.java
@@ -2,14 +2,30 @@ package com.higley.innovativealarmclock;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends Activity {
+	
+	Button button_simplePuzzle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.view_home);
+		
+		// Set up event listeners
+		button_simplePuzzle = (Button)findViewById(R.id.home_buttonSimplePuzzle);
+		button_simplePuzzle.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v) {
+				// Launch activity
+				Intent simplePuzzleIntent = new Intent(Home.this, SimplePuzzle.class);
+				startActivity(simplePuzzleIntent);
+			}
+		});
 	}
 
 	@Override
